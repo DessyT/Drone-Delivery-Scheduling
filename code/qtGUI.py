c8563d32f7058a899d9decaf82786986a7f4329c
Andrew Trail <andrewtrail@googlemail.com>
2020-01-04 13:55:10 +0000

    Add dialog created. Accepts just coords for now. Returns to main form

--- code/qtGUI.py
+++ code/qtGUI.py
@@ -14,10 +14,12 @@ class SchedulerUI(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.bla = 0
 
     def initUI(self):
 
-        #widget = QWidget()
+        self.setGeometry(50,50,800,800)
+        self.setWindowTitle("Delivery Scheduler")
 
         self.lblIntro = QLabel(self)
         self.lblIntro.setText("Welcome to your Delivery Scheduler")
@@ -59,17 +61,97 @@ class SchedulerUI(QWidget):
         self.view.load(QtCore.QUrl().fromLocalFile(path))
 
         print(path)
-        self.setGeometry(50,50,800,800)
-        self.setWindowTitle("Delivery Scheduler")
+
         self.show()
 
     def btnAddClicked(self):
-        print("ADD")
 
-'''
+        '''Testing
+        if self.bla == 0:
+            path = os.path.split(os.path.abspath(__file__))[0]+r'/html/my_map2.html'
+            self.view.load(QtCore.QUrl().fromLocalFile(path))
+
+            self.bla = 1
+        else:
+            path = os.path.split(os.path.abspath(__file__))[0]+r'/html/my_map1.html'
+            self.view.load(QtCore.QUrl().fromLocalFile(path))
+
+            self.bla = 0
+        '''
+
+        #Call our dialog box
+        dlg = AddDialog()
+        if dlg.exec_():
+            #Get data from dialog box
+            coords = dlg.getLocs()
+            print(coords)
+
+        self.lblIntro.setText(coords)
+        #Update GUI with new data
+        self.update()
+
+#New delivery input dialog
+class AddDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    #Layout
+    def initUI(self):
+
+        self.setGeometry(50,50,250,250)
+        self.setWindowTitle("Delivery Scheduler")
+
+        self.txtLat = QLineEdit(self)
+        self.txtLon = QLineEdit(self)
+        #self.txtLat.move(50,50)
+        #self.txtLon.move(50,100)
+
+        QBtn = QDialogButtonBox.Save | QDialogButtonBox.Cancel
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.txtLat)
+        self.layout.addWidget(self.txtLon)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
+    #Return coordinates to main form
+    def getLocs(self):
+
+        lat = self.txtLat.text()
+        lon = self.txtLon.text()
+        coords = lat + "," + lon
+
+        return coords
+
+        '''
+        self.btnAdd = QPushButton(self)
+        self.btnAdd.setText("Add")
+        self.btnAdd.move(50,150)
+        self.btnAdd.clicked.connect(self.btnAddClicked)
+
+        self.btnCancel = QPushButton(self)
+        self.btnCancel.setText("Cancel")
+        self.btnCancel.move(150,150)
+        self.btnCancel.clicked.connect(self.btnCancelClicked)
+        '''
+
+        '''
+    #Button functions
+    def btnAddClicked(self):
+        print("Added")
+        self.close()
+    def btnCancelClicked(self):
+        print("Cancelled")
+        self.close()
+        '''
+
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     test = SchedulerUI()
     sys.exit(app.exec())
-'''
