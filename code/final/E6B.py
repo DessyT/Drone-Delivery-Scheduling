9cf176b8c666b1314b076b3bd0bd48e3a3a64e82
Andrew Trail <andrewtrail@googlemail.com>
2020-02-24 13:14:29 +0000

    Edited for readibility and useability

--- code/final/E6B.py
+++ code/final/E6B.py
@@ -2,24 +2,33 @@ import math as m
 #Calculates the actual angle and speed the drone will have accounting for wind
 
 #Sample wind speed and bearing
-windSpeed = 8
-windDir = m.radians(110)
+windSpeed = 10
+windDir = m.radians(5)
+
+'''
+print("pre",m.degrees(windDir))
+if(m.degrees(windDir) > 360):
+    windDir = windDir - m.radians(360)
+
+print("post",m.degrees(windDir))'''
 #Sample route direction
-routeDir = m.radians(30)
+droneDir = m.radians(300)
 
 #Actual wind speed of drone (max speed)
-actualSpeed = 15
+droneSpeed = 15
 
-angle = m.asin(windSpeed * m.sin(windDir - routeDir) / actualSpeed)
-print("Difference in degrees =",m.degrees(angle))
+angle = m.asin(windSpeed * m.sin(windDir - droneDir) / droneSpeed)
+print("Drone direction =",m.degrees(droneDir))
+print("Wind from =",m.degrees(windDir))
+print("Degree adjustment  =", float(m.degrees(angle)))
 
-heading = routeDir - angle
-print("Direction of travel =", m.degrees(heading))
+heading = droneDir + angle
+print("Direction of travel =", float(m.degrees(heading)))
 
 
 ### Now lets get ground speed
 
-groundSpeed = m.sqrt(m.pow(actualSpeed,2) + m.pow(windSpeed,2) - 2 *
-                    actualSpeed * windSpeed * m.cos(routeDir - windDir + angle))
+groundSpeed = m.sqrt(m.pow(droneSpeed,2) + m.pow(windSpeed,2) - 2 *
+                    droneSpeed * windSpeed * m.cos(droneDir - windDir + angle))
 
 print("Ground speed =",groundSpeed, "m/s")
