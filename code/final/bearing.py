4bedfcbf457d492f66082be51167e57a45bcf511
Andrew Trail <andrewtrail@googlemail.com>
2020-05-05 16:28:20 +0100

    Added a bunch of diagrams. Implementation section completed

--- code/final/bearing.py
+++ code/final/bearing.py
@@ -1,25 +1,6 @@
 
 import math as m
 
-'''
-lat1,lon1,lat2,lon2 = 57.1497, -2.0943, 52.5200, -13.4050
-print("OUT")
-print(lat1)
-print(lon1)
-print(lat2)
-print(lon2)
-lonDiff = m.radians(lon2) - m.radians(lon1)
-latDiff = lat2 - lat1
-y = m.sin(lonDiff) * m.cos(m.radians(lat2))
-x = m.cos(m.radians(lat1)) * m.sin(m.radians(lat2)) - m.sin(m.radians(lat1)) * m.cos(m.radians(lat2)) * m.cos(lonDiff)
-
-bearing = m.degrees(m.atan2(y,x))
-
-if (bearing < 0):
-    bearing += 360
-
-print("plz",bearing)
-'''
 class BearingFinder():
     def __init__(self):
         pass
